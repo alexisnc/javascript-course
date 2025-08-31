95c9016336dfe997a80434b7c15c832c242ebf93
Alexis <acovingt93@gmail.com>
2025-08-31 17:24:32 -0400

    looping objects

--- 09 data-structures-operators/starter/script.js
+++ 09 data-structures-operators/starter/script.js
@@ -60,6 +60,30 @@ if (restaurant.openingHours && restaurant.openingHours.mon) console.log
 
 //console.log(restaurant.openingHours.mon.open);
 
+//Property NAMES 
+const properties = Object.keys(openingHours);
+console.log(properties); 
+
+let openStr = `We are open on ${properties.length} days: `;
+for (const day of properties) {
+  openStr += `${day},`;
+}
+console.log(openStr);
+
+//Property VALUES
+const values = Object.values(openingHours);
+console.log(values); 
+
+//Entire Object
+const entries = Object.entries(openingHours);
+//console.log(entries);
+
+//[key, value]
+ for(const [key, {open, close}] of entries){
+  console.log(`On ${key} we open at ${open} and close at ${close}`);
+ }
+
+
 //WITH optional chaining
 console.log(restaurant.openingHours.mon?.open);
 console.log(restaurant.openingHours?.mon?.open);
